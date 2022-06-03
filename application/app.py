8201f1d41dbd263e7f5ad23a068410feb078d460
admin <bstoneill@yahoo.com.au>
2022-06-03 13:04:41 +1000

    IUpdate

--- application/app.py
+++ application/app.py
@@ -465,7 +465,7 @@ def update(window: pg.GraphicsLayoutWidget, plot: pg.graphicsItems.PlotItem) -> 
 
         iteration += 1
 
-        if iteration % 2 == 0:
+        if iteration % 6 == 0:
             # Pass Machine Learning DBSCAN from Grouper.py:
             (
                 groupCentreX,
@@ -481,7 +481,7 @@ def update(window: pg.GraphicsLayoutWidget, plot: pg.graphicsItems.PlotItem) -> 
             centreY.append(groupCentreY)
 
         try:
-            if iteration % 15 == 0 and num_clusters > 0:
+            if iteration % 30 == 0 and num_clusters > 0:
                 update_occupancy(num_clusters)
                 onlineDash.send_occupancy()
                 velSum.clear()
@@ -496,6 +496,7 @@ def update(window: pg.GraphicsLayoutWidget, plot: pg.graphicsItems.PlotItem) -> 
                     for j in range(num_clusters):
                         velSum[j] = sum(velList[i]) / len(velList[i])
                         angSum[j] = sum(angList[i]) / len(angList[i])
+                
 
                 for i in range(num_clusters):
                     if groupCentreX != 0 and groupCentreY != 0:
